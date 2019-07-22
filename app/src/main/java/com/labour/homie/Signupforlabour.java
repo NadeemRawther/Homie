e0364c2eeaab65e4217b212fc584b5a865f16d94
nadeem <nadeemsani786@hotmail.com>
2019-07-22 16:07:05 +0530

    jhho

--- app/src/main/java/com/labour/homie/Signupforlabour.java
+++ app/src/main/java/com/labour/homie/Signupforlabour.java
@@ -2,10 +2,22 @@ package com.labour.homie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Signupforlabour extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("users/user");
+
+
+
 EditText userid,name,password;
+String names,passwords,userids;
+Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +25,21 @@ EditText userid,name,password;
         userid = (EditText)findViewById(R.id.useriduser);
         name = (EditText)findViewById(R.id.nameuser);
         password = (EditText)findViewById(R.id.passuser);
+        submit = (Button)findViewById(R.id.submitforuser);
 
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               names = name.getText().toString();
+               passwords = password.getText().toString();
+               userids = userid.getText().toString();
+                               UserCreation userCreation = new UserCreation(names,userids,passwords);
+                               myRef.child(userids).setValue(userCreation);
+            }
+        });
 
 
 
